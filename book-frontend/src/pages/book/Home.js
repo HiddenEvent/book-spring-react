7808d9f0aa4b507888e6eccaa07d104bed4278ef
RichardKim <hiddeneventor@gmail.com>
2021-05-04 00:08:54 +0900

    CORS 정책 풀고, React => 서버와 통신

--- book-frontend/src/pages/book/Home.js
+++ book-frontend/src/pages/book/Home.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BookItem from '../../components/BookItem';
 
 const Home = () => {
+  const[books, setBooks] = useState([]);
+
+  // 함수 실행시 최초 한번 실행되는 것
+  useEffect(() => {
+    fetch("http://localhost:8085/book")
+    .then(res=> res.json())
+    .then(res=>{
+      console.log(1, res)
+    }); // 비동기 함수
+  }, [books])
+
+
     return (
         <div>
           <BookItem/>
